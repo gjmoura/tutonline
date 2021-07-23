900379b050f6ac98b64ce647b735fe48b49d3722
Gustavo Jeronimo <gustavo.jeronimo@hotmail.com>
2021-07-23 18:04:33 -0300

    Adiciona mais objetos para exibição

--- routes/search.js
+++ routes/search.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const Tutor = require('../classes/Tutor')
-const Sala = require('../classes/Sala')
+const Tutor = require('../classes/Tutor');
+const Sala = require('../classes/Sala');
 
-const tutor = new Tutor("Gustavo", "gustavo@gmail.com", "Programação", "Dev", "password");
-tutor.setSalasCriadas('Desenvolvimento Web','Programação', 'Sala para programação Web', 'Rocketseat')
-console.log(tutor.getTutor())
+const tutores = []
 
-const competencias = ['Eletrônica', 'Programação', 'Engenharia', 'Design']
+tutores.push(new Tutor("Gustavo", "gustavo@gmail.com", "Programação", "Dev", "password"));
+tutores[0].setSalasCriadas('Desenvolvimento Web','Programação', 'Sala para programação Web', 'Rocketseat');
+tutores.push(new Tutor("Adson", "adson@gmail.com", "Eletrônica", "Doutor", "password"));
+tutores[1].setSalasCriadas('Diodos','Eletrônica', 'Sala de Eletrônica', 'BOYSLTAD');
+tutores.push(new Tutor("Maria Jamilli", "mariajamilli@gmail.com", "Programação", "Doutora", "password"));
+tutores[2].setSalasCriadas('Data Science','Programação', 'Sala para Data Science', 'Data Science');
+tutores.push(new Tutor("Caio Matheus", "mariajamilli@gmail.com", "Matemática", "Doutora", "password"));
+tutores[3].setSalasCriadas('Cálculo','caio', 'Sala para Cálculo', 'Professor Vital');
+tutores.push(new Tutor("Caio Matheus", "caio@gmail.com", "Matemática", "Doutora", "password"));
+tutores[4].setSalasCriadas('Cálculo','Matemática', 'Sala para Cálculo', 'Professor Vital');
+tutores.push(new Tutor("Caio Matheus", "caio@gmail.com", "Matemática", "Doutora", "password"));
+tutores[5].setSalasCriadas('Cálculo','Matemática', 'Sala para Cálculo', 'Professor Vital');
+
+console.log(tutores);
+
+const competencias = ['Eletrônica', 'Programação', 'Engenharia', 'Design'];
 
 router.get('/', (req,res) => {
-  res.render('search', { competencias:  competencias, tutor: tutor})
+  res.render('search', { competencias:  competencias, tutores: tutores});
 })
 
 module.exports = router;
